a63e0db879c3b6bb3e71672072ef4aeaffc2c095
Carolineliu <fruitsugar1@outlook.com>
2015-08-21 20:49:59 +0800

    finish function submit

--- routes/submit.js
+++ routes/submit.js
@@ -2,22 +2,28 @@ var express = require('express');
 var models = require('../models');
 var router = express.Router();
 var answers = models.answers;
+var answer = models.answered_papers;
 
 router.get('/', function(req, res) {
   res.sendfile("./views/page.html");
 });
+
 router.get('/getform', function(req, res) {
+  var array = [];
   for (var i = 0; i < req.query.data.length; i++) {
     var id = (req.query.data)[i].name;
-    var array = [];
+    array.push(id);
     var obj = (req.query.data[i].value).join(",");
     var user = answers.create({
       question_id: id,
       types: req.query.data[i].type,
       answer_context: obj
-    }).done(function() {
-
     });
   }
+
+  var objs = array.join(",");
+  var users = answer.create({
+    answers_array: objs
+  });
 });
 module.exports = router;
