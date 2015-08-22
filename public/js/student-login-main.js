10246fe206921a9ac5f9f2e647171bb2c5cd4831
Chrissie <urnotchrissie@outlook.com>
2015-08-22 18:53:52 +0800

    fix bug

--- public/js/student-login-main.js
+++ public/js/student-login-main.js
@@ -5,12 +5,15 @@ $('body').on('click', "#button", function() {
 
   if (username.length !== 8 || isNaN(parseInt(username))) {
     $(".un-error").html("请输入8位数字的学号~");
+    $("#username").val("");
   } else if (password.length === 0) {
     $(".un-error").html("");
     $(".pw-error").html("请输入密码～");
   } else if (password.length < 6 || password.length > 12) {
     $(".un-error").html("");
     $(".pw-error").html("用户名不存在或密码不正确哦～");
+    $("#username").val("");
+    $("#password").val("");
   } else {
     $(".pw-error").html("");
     $.ajax({
@@ -28,6 +31,8 @@ $('body').on('click', "#button", function() {
         }
         else {
           $(".pw-error").html("用户名不存在或密码不正确哦");
+          $("#username").val("");
+          $("#password").val("");
         }
       }
     });
