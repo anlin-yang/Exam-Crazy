766d31b93ed89558335a2f62e78f6f3079913577
songjianhua2012 <songjianhua2012@outlook.com>
2015-08-22 17:55:54 +0800

    change app.js

--- public/js/teacher-check-page.js
+++ public/js/teacher-check-page.js
@@ -1,6 +1,6 @@
 $(function() {
   $('.checkpage').on('click',function() {
-    $.get('teacher-check/check.html', function(resp) {
+    $.get('teacherCheck/check.html', function(resp) {
       showcheckpage(resp);
     });
   });
@@ -10,6 +10,6 @@ function showcheckpage(str) {
   $('.right').empty();
   str.map(function(val) {
     var temp = val.paper_name;
-    $('.right').append("<tr><td><a target=_blank href=teacher-check/page?name="+temp+">" +val.paper_name+'</a></td><tr>');
+    $('.right').append("<tr><td><a target=_blank href=teacherCheck/page?name="+temp+">" +val.paper_name+'</a></td><tr>');
   });
 }
