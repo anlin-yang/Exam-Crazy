3a9e9c8117e5f40a7d2a3b468c33ed4db40f8aa7
songjianhua2012 <songjianhua2012@outlook.com>
2015-08-22 16:10:28 +0800

    remove unuseful files

--- routes/teacher-check.js
+++ routes/teacher-check.js
@@ -21,11 +21,12 @@ router.get('/check.html',function(req,res) {
 router.get('/page',function(req,res) {
   var temp = req.query.name;
   var result = [];
-  // models.papers.find({where:{paper_name:temp}}).then(function(data) {
-  //  changQuestionArray(data);
-  //  models.questions.find({where:{question_id:data.question_array}});
-  //  result.push();
+   models.papers.find({where:{paper_name:temp}}).then(function(data) {
+   //changQuestionArray(data);
+   console.log(data);
+   //models.questions.find({where:{question_id:data.question_array}});
+   //result.push();
    res.render('page.hbs');
-//  });
+  });
 });
  module.exports = router;
