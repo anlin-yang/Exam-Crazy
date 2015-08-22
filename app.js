84acc162bd1fc71d6d8bd9b04b15a199addef845
Carolineliu <fruitsugar1@outlook.com>
2015-08-22 19:01:13 +0800

    add router in app.js

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use('/answerPaper', answerPaper);
 app.use('/teacherLogin', teacherLogin);
 app.use('/addSingle',addsingle);
 app.use('/teacherPaper',teacherPaper);
+app.use('/teacher',teacherPaper);
 
 app.listen(3000);
 console.log("Listening on port 3000:http://localhost:3000");
