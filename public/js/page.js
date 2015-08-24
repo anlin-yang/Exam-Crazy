029f790cb0c5bfa61855e278902d384b81315dd7
songjianhua2012 <songjianhua2012@outlook.com>
2015-08-24 11:34:19 +0800

    add page.js

--- public/js/page.js
+++ public/js/page.js
@@ -1,3 +1,8 @@
 $(function() {
-  
+  $('#suretocheck').on('click',function() {
+    var temp = $(this).data('name');
+    $.get('teacher-check/suretocheck',{name:temp},function(resp) {
+      
+    });
+  });
 });
--- routes/teacher-check.js
+++ routes/teacher-check.js
@@ -18,7 +18,7 @@ router.get('/check.html',function(req,res) {
     });
 });
 
-router.get('/page',function(req,res) {
+router.get('/suretocheck',function(req,res) {
   var temp = req.query.name;
   var result = [];
   var a = [];
@@ -45,8 +45,40 @@ router.get('/page',function(req,res) {
        });
       }
    });
-   res.render('page.hbs',{array:a});
+   res.send(a);
   });
+
+});
+
+router.get('/page',function(req,res) {
+  var temp = req.query.name;
+  var result = [];
+  var a = [];
+  a.push({"title":temp});
+  //  models.papers.find({where:{paper_name:temp}}).then(function(data) {
+  //    var str = data.dataValues.question_array;
+  //    var arr = str.split(/\[|\]|,/);
+  //    a.push({title:temp});
+  //  arr.forEach(function(val) {
+  //     if(val.length !== 0) {
+  //      models.questions.find({where:{question_id:parseInt(val)}}).then(function(data) {
+  //        var b = [];
+  //        b = data.dataValues.question_content.split('-');
+   //
+  //       models.types.find({where:{type_id:data.dataValues.type_id}}).then(function(data) {
+  //         //a.push({type:data.dataValues.type});
+  //         if(data.dataValues.type === '选择题') {
+  //           a.push({"type":data.dataValues.type,"question_content":b[0],"answer_A":b[1],"answer_B":b[2],"answer_C":b[3],"answer_D":b[4]});
+  //         } else {
+  //           a.push({"type":data.dataValues.type,question_content:b[0]});
+  //         }
+  //         console.log(a);
+  //       });
+  //      });
+  //     }
+  //  });
+   res.render('page.hbs',{array:a});
+//  });
 
 
 });
