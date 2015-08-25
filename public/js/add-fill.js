277564cd9bb428ec063a46968c6a0b657a6e7e36
zwimeyihmh <zwimeyihmh@outlook.com>
2015-08-25 09:37:52 +0800

    add js to get value to add fill

--- public/js/add-fill.js
+++ public/js/add-fill.js
@@ -1,58 +1,54 @@
 $(function() {
-  var temp = 0;
-
   $('#fill_answers').click(function() {
-    temp ++;
+    $('#answer').html('');
     var question = $('#fill_question').val();
     if (question.length === 0) {
       $('#judge1').html('还没有写题目！');
     } else {
-      console.log(question);
-      if (temp > 0) {
-        console.log($('#fill_question').val());
-        console.log(question);
-        if ($('#fill_question').val() !== question) {
-          $('#judge1').html('');
-          var blank = question.split("_");
-          var count = blank.length - 1;
-          var str = '';
-          while (count--) {
-            str = '<input type="text" class="input form-group" id="fill" name="fill"><br>';
-            $('#add_fill').append(str);
+      $('#judge1').html('');
+      var blank = question.split("_");
+      var count = blank.length - 1;
+      var fillBlacks = '';
+      for (var i = 0; i < count; i++) {
+        fillBlacks += '<input type="text" class="input form-group" id="inputAnswers" name="inputAnswers" value=""><br>';
+      }
+      fillBlacks += '<input type="button" class="btn btn-primary form-group" id="submitFill" name="name" value="提交">';
+
+      $('#answers').html(fillBlacks);
+
+      $("#submitFill").click(function() {
+        var inputAnswers = $("[name='inputAnswers']");
+        var temp = 0;
+        var answers = [];
+
+        for (var x = 0; x < inputAnswers.length; x++) {
+          if (inputAnswers[x].value.length === 0) {
+            $('#answer').html('请输入完整的答案！');
+            temp = 1;
+            break;
+          } else {
+            answers.push(inputAnswers[x].value);
           }
         }
 
-      }
+        if (temp === 0) {
+          $('#answer').html('');
+          var fill = {
+            question: question,
+            answers: answers
+          };
+          $.ajax({
+            type: 'POST',
+            url: '/addFill/fill',
+            data: {
+              fill: fill
+            },
+            success: function(result) {}
+          });
+
+        }
+
+      });
     }
   });
-
-
-
-  // var i = 0;
-  // while(count--) {
-  //   str.push({answer:ss});
-  // }
-  // $.ajax({
-  //   url: '/addFill/fillAnswers',
-  //   data: {
-  //     str: str
-  //   },
-  //   type: 'POST',
-  //   success: function() {
-  //     var answers = $("[name='fill']");
-  //     if (answers.length === str.length) {
-  //       $('#surefill').click(function() {
-  //         for (var i = 0; i < answers.length; i++) {
-  //           if (answers[i].length === 0) {
-  //
-  //           }
-  //         }
-  //       });
-  //     }
-  //   }
-  // });
-
-
-
-
 });
