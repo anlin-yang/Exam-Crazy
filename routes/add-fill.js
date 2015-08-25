639babba31774c14e8f65691e9ec22c62e939c50
zwimeyihmh <zwimeyihmh@outlook.com>
2015-08-25 09:37:08 +0800

    add route to add fill to connect database

--- routes/add-fill.js
+++ routes/add-fill.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var models = require('../models');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('add-fill', {
     title: 'Express',
-    str: []
   });
 });
 
-// router.post('/fillAnswers', function(req, res, next) {
-//   var str = req.body.str;
-//   console.log(req.body.str);
-//   res.render('add-fill', {
-//     title: 'Express',
-//     str: str,
-//   });
-// });
+router.post('/fill', function(req, res, next) {
+  var fill = req.body.fill;
+  models.Question.create({
+    id:1,
+    typeId: 3,
+    questionContent: fill.question,
+    questionKey:fill.answers[0],
+    questionPoint:1
+  });
+});
 
 module.exports = router;
