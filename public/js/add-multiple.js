be9b037207946efa00f3cdbb18d0ac60c9752bc1
wy <wy08271@outlook.com>
2015-08-25 11:43:41 +0800

    modify add-multiple.js

--- public/js/add-multiple.js
+++ public/js/add-multiple.js
@@ -7,32 +7,37 @@ $(function() {
   });
   $('body').on('click', "#sure", function() {
 
-    var question = $("#question").val();
-    var A = $("#A").val();
-    var B = $("#B").val();
-    var C = $("#C").val();
-    var D = $("#D").val();
+    var array = ['#question', '#A', '#B', '#C', '#D'],
+      value = [];
+    for (var i = 0; i < array.length; i++) {
+      value[i] = $(array[i]).val();
+    }
+
     var item = $("[name='answer']");
     var answer = '';
-    for (var i = 0; i < item.length; i++) {
-      if (item[i].checked) {
-        answer += item[i].value;
+    for (var x = 0; x < item.length; x++) {
+      if (item[x].checked) {
+        answer += item[x].value;
       }
     }
 
-    if (!(question)) {
-      $("#questionPrompt").html("问题忘记填写！");
-    } else if (!(A)) {
-      $("#optionA").html("A选项忘记填写！");
-    } else if (!(B)) {
-      $("#optionB").html("B选项忘记填写！");
-    } else if (!(C)) {
-      $("#optionC").html("C选项忘记填写！");
-    } else if (!(D)) {
-      $("#optionD").html("D选项忘记填写！");
-    } else {
+    var questionInformation = '',
+      questionAnswer = '',
+      count = 0;
+
+    for (var k = 0; k < array.length; k++) {
+      if (!(value[k])) {
+        var itemOne = array[k] + 'option';
+        $(itemOne).show();
+        $(itemOne).delay(3000).hide(1);
+      } else {
+        questionInformation += value[k] + '-';
+        count++;
+      }
+    }
+    if (count === 5) {
       var information = {
-        questionInformation: question + '-' + A + '-' + B + '-' + C + '-' + D,
+        questionInformation: questionInformation,
         questionAnswer: answer
       };
 
@@ -43,13 +48,12 @@ $(function() {
         success: function(successfulTip) {
           console.log(successfulTip);
           $('.input').val("");
-
           [].forEach.call($("[name='answer']"), function(elem) {
             elem.checked = false;
-          });
 
-          $("#addBox").show();
-          $("#addBox").delay(3000).hide(1);
+            $("#addBox").show();
+            $("#addBox").delay(3000).hide(1);
+          });
         }
       });
     }
