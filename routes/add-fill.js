b6ab6ecfac2d927b4ff5f8abc27238b01de53f28
zwimeyihmh <zwimeyihmh@outlook.com>
2015-08-25 12:00:17 +0800

    move addFill of route to controller

--- routes/add-fill.js
+++ routes/add-fill.js
@@ -1,29 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var AddFill = require('../controllers/add-fill.js');
 var models = require('../models');
+var addFill = new AddFill();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('add-fill', {
     title: 'Express',
   });
 });
-
-router.post('/fill', function(req, res) {
-  var fill = req.body.fill;
-  models.Question.create({
-    typeId: 3,
-    questionContent: fill.question,
-    questionKey: fill.answers,
-    questionPoint: fill.count
-  }).then(function(data) {
-    console.log(data.dataValues);
-    if (data.dataValues.id > 0) {
-      res.send({
-        question:data.dataValues.id,
-        status: 200
-      });
-    }
-  });
-});
+router.post('/fill', addFill.add);
 
 module.exports = router;
