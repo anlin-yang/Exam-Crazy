e9d12b07cf621bc054b4de9b33636ad9656e8693
zwimeyihmh <zwimeyihmh@outlook.com>
2015-08-25 12:00:42 +0800

    add AddFill

---
+++ controllers/add-fill.js
@@ -0,0 +1,26 @@
+var models = require('../models');
+
+function AddFill() {
+
+}
+AddFill.prototype.add = function(req,res) {
+  var fill = req.body.fill;
+  models.Question.create({
+    typeId: 3,
+    questionContent: fill.question,
+    questionKey: fill.answers,
+    questionPoint: fill.count
+  }).then(function(data) {
+    if (data.dataValues.id > 0) {
+      res.send({
+        question:data.dataValues.id,
+        status: 200
+      });
+    } else {
+      res.send({
+        status:501
+      });
+    }
+  });
+};
+module.exports = AddFill;
