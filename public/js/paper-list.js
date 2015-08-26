f7997daed9bfacfc291b4ef4f70f92e256f43ed5
jian <Jiandandian012@outlook.com>
2015-08-26 14:47:36 +0800

    finished paper-list.js

--- public/js/paper-list.js
+++ public/js/paper-list.js
@@ -1,16 +1,14 @@
 $(function() {
   $('#paperList tbody tr').on('click', '#deal', function() {
     var deal = $(this).eq(0).text().trim();
-    if (deal === '查看详情') {
-      $.get('', {
-
-      }, function(result) {});
-    } else {
-      $.get('', {
-
-      }, function(result) {
-
+    var PaperItem = $(this).parent().children();
+    var PaperId = PaperItem.eq(0).text().trim();
+    if (deal === '点击答题') {
+      $.get('/student-answer',{data:PaperId},function(){
+        
       });
-    }
+
+      }
+
   });
 });
