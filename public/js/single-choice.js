34d4dd16a3bec4560337e3dd38f2574170638f51
wy <wy08271@outlook.com>
2015-08-26 15:10:28 +0800

    add single-choicd.js function

--- public/js/single-choice.js
+++ public/js/single-choice.js
@@ -1,58 +1,59 @@
-$(function() {
-  $('body').on('click', "#singleEmpty", function() {
-    $('.input').val("");
-    [].forEach.call($("[name='singleAnswer']"), function(elem) {
-      elem.checked = false;
-    });
-  });
-
-  $('body').on('click', "#singleSure", function() {
-
-    var allsingle = $("[name='singleQuestion']");
-    var item = $("[name='singleAnswer']");
-    var answer;
-    for (var x = 0; x < item.length; x++) {
-      if (item[x].checked) {
-        answer = item[x].value;
-        break;
-      }
-    }
-
-    var questionInformation = '',questionAnswer = '',count = 0;
-    var option =['question','A','B','C','D'];
-
-    for (var k = 0; k < allsingle.length; k++) {
-      if (!(allsingle[k].value)) {
-
-        var itemOne = '#' + option[k] + 'singleoption';
-        $(itemOne).show();
-        $(itemOne).delay(3000).hide(1);
-      } else {
-        questionInformation += allsingle[k].value + '-';
-        count++;
-      }
-    }
-    if (count === allsingle.length) {
-      var information = {
-        questionInformation: questionInformation,
-        questionAnswer: answer
-      };
-
-      $.ajax({
-        url: '/singleChoice',
-        data: information,
-        type: 'POST',
-        success: function(successfulTip) {
-          console.log(successfulTip);
-          $('.input').val("");
-          [].forEach.call($("[name='singleAnswer']"), function(elem) {
-            elem.checked = false;
-
-            $("#addBox").show();
-            $("#addBox").delay(3000).hide(1);
-          });
-        }
+function singlechoice(){
+      $('#singleEmpty').click(function(){
+      $('.input').val("");
+      [].forEach.call($("[name='singleAnswer']"), function(elem) {
+        elem.checked = false;
       });
-    }
-  });
-});
+    });
+
+    $('#singleSure').click(function(){
+
+      var allsingle = $("[name='singleQuestion']");
+      var item = $("[name='singleAnswer']");
+      var answer;
+      for (var x = 0; x < item.length; x++) {
+        if (item[x].checked) {
+          answer = item[x].value;
+          break;
+        }
+      }
+
+      var questionInformation = '',questionAnswer = '',count = 0;
+      var option =['question','A','B','C','D'];
+
+      for (var k = 0; k < allsingle.length; k++) {
+        if (!(allsingle[k].value)) {
+
+          var itemOne = '#' + option[k] + 'singleoption';
+          $(itemOne).show();
+          $(itemOne).delay(3000).hide(1);
+        } else {
+          questionInformation += allsingle[k].value + '-';
+          count++;
+        }
+      }
+      if (count === allsingle.length) {
+        var information = {
+          questionInformation: questionInformation,
+          questionAnswer: answer
+        };
+
+        $.ajax({
+          url: 'teacher/singleChoice',
+          data: information,
+          type: 'POST',
+          success: function(successfulTip) {
+            console.log(successfulTip);
+            $('.input').val("");
+            [].forEach.call($("[name='singleAnswer']"), function(elem) {
+              elem.checked = false;
+
+              $("#addBox").show();
+              $("#addBox").delay(3000).hide(1);
+            });
+          }
+        });
+      }
+    });
+    return successfulTip.singleID;
+}
