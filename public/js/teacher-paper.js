facc5e876256349212e988bbd55a22be1c1bc79a
wy <wy08271@outlook.com>
2015-08-26 08:52:47 +0800

    modify views/add-single.hbs

--- public/js/teacher-paper.js
+++ public/js/teacher-paper.js
@@ -8,7 +8,17 @@ $(function() {
     }, function(msg) {});
   });
 
+<<<<<<< Updated upstream
   $("#addfill").on("click", function() {
+=======
+  $("#single").on("click", function() {
+    $.post("teacher/single", function(data) {
+      $("#addItem").empty();
+      var content=$(data).find("#content");
+      $("#addItem").append(content);
+    });
+  });
+>>>>>>> Stashed changes
 
     // $.post("teacher/fill", function(data) {
       addfill();
