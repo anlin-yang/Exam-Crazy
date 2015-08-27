3799f9f941fc0047f1d668dbbf9aa0d4fc187a02
Carolineliu <fruitsugar1@outlook.com>
2015-08-27 18:56:00 +0800

    change controllers/teacher-paper.js

--- controllers/teacher-paper.js
+++ controllers/teacher-paper.js
@@ -8,95 +8,9 @@ TeacherPaper.getTeacherPaper = function(req, res) {
   res.render('teacher/addQuestions');
 };
 
-TeacherPaper.getAddFill = function(req, res) {
-  var fill = req.body.fill;
-  models.Question.create({
-    typeId: 3,
-    questionContent: fill.question,
-    questionKey: fill.answers,
-    questionPoint: fill.count
-  }).then(function(data) {
-    array.push(data.dataValues.id);
-    if (data.dataValues.id > 0) {
-
-      res.send({
-        question: data.dataValues.id,
-        status: STATUS.DATA_SUCCESS,
-      });
-    } else {
-      res.send({
-        status: STATUS.QUR_ERROR,
-      });
-    }
-  });
-};
-
-TeacherPaper.getAddSingle = function(req, res) {
-  var question = req.body.questionInformation;
-  var answer = req.body.questionAnswer;
-
-  models.Question.create({
-    questionKey: answer,
-    questionContent: question,
-    questionPoint: 1,
-    typeId: 1
-  }).then(function(data) {
-    if (data.length !== 0) {
-      console.log(data.dataValues.id);
-      array.push(data.dataValues.id);
-      res.send({
-        status: STATUS.DATA_SUCCESS,
-        singleID: data.dataValues.id
-      });
-    }
-  });
-};
-
-TeacherPaper.getAddMultiple = function(req, res) {
-  var question = req.body.questionInformation;
-  var answer = req.body.questionAnswer;
-
-  models.Question.create({
-    questionKey: answer,
-    questionContent: question,
-    questionPoint: 1,
-    typeId: 2
-  }).then(function(data) {
-    array.push(data.dataValues.id);
-    if (data.length !== 0) {
-      res.send({
-        status: STATUS.DATA_SUCCESS,
-        multipleID: data.dataValues.id
-      });
-    }
-  });
-};
-
-TeacherPaper.getJudge = function(req, res) {
-
-};
-
-TeacherPaper.getPaperInfo = function(req, res) {
-  var newarray = array.join(",");
-  var user = models.Paper.create({
-    paperName: req.body.paperName,
-    subject: req.body.subject,
-    score: req.body.score,
-    questionArray: newarray
-
-  }).then(function(data) {
-    if (data.length === 0) {
-      res.send({
-        staus: STATUS.QUR_ERROR,
-        data: {}
-      });
-    } else {
-      res.send({
-        staus: STATUS.DATA_SUCCESS,
-        data: {}
-      });
-    }
-  });
+TeacherPaper.addQuestions = function(req, res) {
+  var h=req.body;
+  console.log(h);
 };
 
 module.exports = TeacherPaper;
