357a0d6de75af9734c03992a6f1ebbb98ceb839c
zwimeyihmh <zwimeyihmh@outlook.com>
2015-08-27 09:44:11 +0800

    change function name

--- routes/add-fill.js
+++ routes/add-fill.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var addFill = require('../controllers/add-fill.js');
-router.get('/', addFill.fill);
+router.get('/', addFill.page);
 router.post('/', addFill.addFill);
 
 module.exports = router;
