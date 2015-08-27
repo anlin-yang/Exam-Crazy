59148c00be93a6c1521f88b935be2a3d2ae84408
Carolineliu <fruitsugar1@outlook.com>
2015-08-27 18:57:10 +0800

    change routes/teacher-paper.js

--- routes/teacher-paper.js
+++ routes/teacher-paper.js
@@ -4,14 +4,6 @@ var TeacherPaper = require("../controllers/teacher-paper.js");
 
 router.get("/", TeacherPaper.getTeacherPaper);
 
-router.post("/fill", TeacherPaper.getAddFill);
-
-router.post("/singleChoice", TeacherPaper.getAddSingle);
-
-router.post("/multipleChoice", TeacherPaper.getAddMultiple);
-
-router.post("/judge", TeacherPaper.getJudge);
-
-router.post("/",TeacherPaper.getPaperInfo);
+router.post("/question", TeacherPaper.addQuestions);
 
 module.exports = router;
