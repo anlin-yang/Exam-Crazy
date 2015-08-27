52a05ec444d3d467d7ecddcdbdc709cb682161b0
zwimeyihmh <zwimeyihmh@outlook.com>
2015-08-27 16:33:18 +0800

    rewrite deal with the fill add

--- public/js/add-fill.js
+++ public/js/add-fill.js
@@ -1,94 +1,24 @@
-function addFill() {
-  $(function() {
-    $('#fillAnswers').click(function() {
-      $('#fillAnswerJudge').html('');
-      var question = $('#fillQuestion').val();
-      if (question.length === 0) {
-        $('#fillQusetionJudge').html(fillStatus.FILL_QUESTION_NULL);
-      } else {
-        $('#fillQusetionJudge').html('');
-        var blank = question.split("_");
-        var count = blank.length - 1;
-        var fillBlacks = '';
-        for (var i = 0; i < count; i++) {
-          fillBlacks += '<input type="text" class="input form-group" id="inputFillAnswers" name="inputFillAnswers" value=""><br>';
-        }
-        if (i <= 0) {
-          $('#fillQusetionJudge').html(fillStatus.FILL_BLANK_NULL);
+$(function () {
+    $('#submitFill').on('click', function () {
+        var question = $('#fillQuestion').val();
+        var answer = $('#fillAnswer').val();
+        if (question.length === 0) {
+            $('#fillQusetionJudge').html(fillStatus.FILL_QUESTION_NULL);
         } else {
-          fillBlacks += '<input type="button" class="btn btn-primary form-group" id="submitFill" name="name" value="提交">';
+            $('#fillQusetionJudge').html('');
         }
-        $('#fillBlank').html(fillBlacks);
-        $("#submitFill").click(function() {
-          var inputAnswers = $("[name='inputFillAnswers']");
-          var temp = 0;
-          var answersCount = 0;
-          var answers = [];
-          for (var x = 0; x < inputAnswers.length; x++) {
-            if (inputAnswers[x].value.length === 0) {
-              $('#fillAnswerJudge').html(fillStatus.FILL_ANSWERS_INCOMPLETE);
-              temp = 1;
-              break;
-            } else {
-              answers.push(inputAnswers[x].value);
-              answersCount++;
-            }
-          }
-
-
-          if (temp === 0) {
+        if (answer.length === 0) {
+            $('#fillAnswerJudge').html(fillStatus.FILL_ANSWERS_INCOMPLETE);
+        } else {
             $('#fillAnswerJudge').html('');
-            var strAnswers = JSON.stringify(answers);
-            var fill = {
-              id: 3,
-              question: question,
-              answers: strAnswers,
-              point: answersCount
-            };
-
-            $.ajax({
-              type: 'POST',
-              url: '/fill',
-              data: {
-                fill: fill
-              },
-              success: function(result) {
-                if (result.status === STATUS.DATA_SUCCESS) {
-                  fillQuestionId = result.questionId;
-                  $('#submitResult').show();
-                  $('#fillBlank').html('');
-                  $('#fillQuestion').val('');
-                  $('#submitResult').delay(3000).hide(1);
-                } else if (result.status === STATUS.INS_ERROR) {
-                  $('#submitResult').html(fillStatus.FILL_INSERT_FAIL);
-                  $('#submitResult').delay(3000).hide(1);
-                }
-                return fillQuestionId;
-              }
-            });
-            $.ajax({
-              type: 'POST',
-              url: 'teacher/fill',
-              data: {
-                fill: fill
-              },
-              success: function(result) {
-                var realanswers = answers.split("-");
-                if (result.status === STATUS.DATA_SUCCESS) {
-                  realanswers.forEach(function(val) {
-                    $('#showPaper').append("<tr>" +
-                      "<td>" + "题目：" + question + "</td>" +
-                      "<td>" + "答案:" + val + "</td>" + "</tr>");
-                  });
-                } else if (result.status === STATUS.INS_ERROR) {
-                  $('#submitSuccess').html(fillStatus.FILL_INSERT_FAIL);
-                }
-              }
-            });
-          }
-        });
-      }
-
-    });
-  });
-}
+        }
+        if (answer.length > 0 && question.length > 0) {
+            var addFill = {
+                typeId: 3,
+                questionContent: question,
+                questionKey: answer,
+                questionPoint: 1
+            }
+        }
+    })
+});
