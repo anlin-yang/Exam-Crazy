6ce281274110586f40062c9268cb43dfe9981356
jian <Jiandandian012@outlook.com>
2015-08-27 19:19:31 +0800

    delete models.Paper column:questionArray

--- models/paper.js
+++ models/paper.js
@@ -7,7 +7,6 @@ module.exports = function(sequelize, DataTypes) {
     },
     paperName: DataTypes.STRING,
     subject: DataTypes.STRING,
-    questionArray: DataTypes.TEXT,
     score: DataTypes.INTEGER
   }, {
     classMethods: {
