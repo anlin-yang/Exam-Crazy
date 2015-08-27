55b276380ab641106158c4d03f95282d6889d076
wy <wy08271@outlook.com>
2015-08-27 20:12:25 +0800

    delete multiple-choice.js ajax request

--- public/js/multiple-choice.js
+++ public/js/multiple-choice.js
@@ -1,66 +1,47 @@
-function addMultipleChoice(){
-
-      $('#multipleEmpty').click(function(){
-      $('.input').val("");
-      [].forEach.call($("[name='multipleAnswer']"), function(elem) {
-        elem.checked = false;
-      });
-    });
-
-    $('#multipleSure').click(function(){
-
-      var allmultiple = $("[name='multipleQuestion']");
-      var item = $("[name='multipleAnswer']");
-      var answer = '';
-      for (var x = 0; x < item.length; x++) {
-        if (item[x].checked) {
-          answer += item[x].value;
-        }
-      }
-
-      var questionInformation = '',questionAnswer = '',count = 0;
-      var htmlInformation ='';
-      var option =['question','A','B','C','D'];
-
-      for (var k = 0; k < allmultiple.length; k++) {
-        if (!(allmultiple[k].value)) {
-          var itemOne = '#' + option[k] + 'multipleoption';
-          $(itemOne).show();
-          $(itemOne).delay(3000).hide(1);
-        } else {
-          htmlInformation += option[k] + ':' + allmultiple[k].value + '<br>';
-          questionInformation += allmultiple[k].value + '-';
-          count++;
-        }
-      }
-      if (count === allmultiple.length) {
-        var information = {
-          questionInformation: questionInformation,
-          questionAnswer: answer
-        };
-
-        $.ajax({
-          url: 'teacher/multipleChoice',
-          data: information,
-          type: 'POST',
-          success: function(successfulTip) {
-            if(successfulTip.status === STATUS.DATA_SUCCESS){
-              successdata = successfulTip.multipleID;
-              $('.input').val("");
-
-              [].forEach.call($("[name='multipleAnswer']"), function(elem) {
-                elem.checked = false;
-              });
-              $('#showPaper').append("<li>" + htmlInformation + "</li>");
-              $("#success").show();
-              $("#success").delay(3000).hide(1);
-            }else if(successfulTip.status === STATUS.INS_ERROR){
-              $("#failed").show();
-              $("#failed").delay(3000).hide(1);
-            }
-          }
+$(function () {
+    $('#multipleEmpty').on('click', function () {
+        $('.input').val("");
+        [].forEach.call($("[name='multipleAnswer']"), function (elem) {
+            elem.checked = false;
         });
-      }
     });
-  
-}
+
+    $('#multipleSure').click(function () {
+        var allmultiple = $("[name='multipleQuestion']");
+        var item = $("[name='multipleAnswer']");
+        var answer;
+        for (var x = 0; x < item.length; x++) {
+            if (item[x].checked) {
+                answer += item[x].value;
+            }
+        }
+
+        var questionInformation = '', questionOptions = [], count = 1;
+        var option = ['question', 'A', 'B', 'C', 'D'];
+
+        for (var m = 0; m < allmultiple.length; m++) {
+            if (!(allmultiple[m].value)) {
+                var itemOne = '#' + option[m] + 'multipleoption';
+                $(itemOne).show();
+                $(itemOne).delay(3000).hide(1);
+            } else {
+                if (m === 0) {
+                    questionInformation = allmultiple[m].value;
+                } else {
+                    questionOptions.push(option[m] + ':' + allmultiple[m].value);
+                    count++;
+                }
+            }
+        }
+
+        if (count === allmultiple.length) {
+            var information = {
+                questionContent: questionInformation,
+                questionKey: answer,
+                questionPoint: 1,
+                typeId: 1,
+                optionContent: questionOptions
+            };
+        }
+    });
+});
