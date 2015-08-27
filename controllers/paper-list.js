a9d1c14ffeb91d9b9137a8dc7af6ce52e8ba5b6a
jian <Jiandandian012@outlook.com>
2015-08-27 09:47:22 +0800

    liuye.

--- controllers/paper-list.js
+++ controllers/paper-list.js
@@ -1,49 +1,45 @@
 var models = require('../models');
+var paperListStatus = require('../public/js/paper-list-status');
 var Score = models.Score;
 var Paper = models.Paper;
+var studentId = 1;
 
+var PaperList = {};
 
-
-function PaperList() {
-
-}
-
-PaperList.prototype.getPaperList = function(req, res) {
+PaperList.getPaperList = function(req, res) {
   var paperList = [];
   var scoreList = [];
-  //var studentId=req.body.studentId;
-  var studentId = 1;
 
   Paper.findAll().then(function(data) {
-    data.forEach(function(val) {
-      paperList.push({
+    paperList = data.map(function(val) {
+      return {
         id: val.dataValues.id,
         paperName: val.dataValues.paperName,
-        status: '未答',
-        deal: '点击答题'
+        status: paperListStatus.NO_ANSWERED_STATUS,
+        deal: paperListStatus.NO_ANSWERED_DEAL,
+        url: 'student-answer'
+      };
+    });
+    return Score.findAll();
+  }).then(function(data) {
+    scoreList = data.map(function(val) {
+      return {
+        paperId: val.dataValues.paperId,
+        studentId: val.dataValues.studentId,
+        score: val.dataValues.score
+      };
+    });
+    paperList.forEach(function(val) {
+      scoreList.forEach(function(item) {
+        if (val.id === item.paperId) {
+          val.status = item.score.toString();
+          val.deal = paperListStatus.ANSWERED_DEAL;
+          val.url = 'paperList';
+        }
       });
     });
-
-    Score.findAll().then(function(data) {
-      data.forEach(function(val) {
-        scoreList.push({
-          paperId: val.dataValues.paperId,
-          studentId: val.dataValues.studentId,
-          score: val.dataValues.score
-        });
-      });
-      paperList.forEach(function(val) {
-        scoreList.forEach(function(item) {
-          if (val.id === item.paperId) {
-            val.status = item.score.toString();
-            val.deal = '查看详情';
-          }
-        });
-      });
-
-      res.render('paper-list', {
-        paperList: paperList
-      });
+    res.render('paper-list', {
+      paperList: paperList
     });
   });
 };
--- routes/paper-list.js
+++ routes/paper-list.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var PaperList = require('../controllers/paper-list.js');
-var paperList = new PaperList();
-router.get('/', paperList.getPaperList);
+router.get('/', PaperList.getPaperList);
 
 module.exports = router;
