ffe1d588ecb7cc39b579e8e44b3c97631fd0953b
wy <wy08271@outlook.com>
2015-08-28 19:40:21 +0800

    delete single-choice & multiple-choice 's EmptyButton

--- public/js/multiple-choice.js
+++ public/js/multiple-choice.js
@@ -1,47 +1,44 @@
-$(function () {
-    $('#multipleEmpty').on('click', function () {
-        $('.input').val("");
-        [].forEach.call($("[name='multipleAnswer']"), function (elem) {
-            elem.checked = false;
-        });
-    });
+$(function() {
 
-    $('#multipleSure').click(function () {
-        var allmultiple = $("[name='multipleQuestion']");
-        var item = $("[name='multipleAnswer']");
-        var answer = '';
-        for (var x = 0; x < item.length; x++) {
-            if (item[x].checked) {
-                answer += item[x].value;
-            }
+  $('#multipleSure').click(function() {
+    var allmultiple = $("[name='multipleQuestion']");
+    var item = $("[name='multipleAnswer']");
+    var answer = '';
+    for (var x = 0; x < item.length; x++) {
+      if (item[x].checked) {
+        answer += item[x].value;
+      }
+    }
+
+    var questionInformation = '',
+      questionOptions = [],
+      count = 1;
+    var option = ['question', 'A', 'B', 'C', 'D'];
+
+    for (var m = 0; m < allmultiple.length; m++) {
+      if (!(allmultiple[m].value)) {
+        var itemOne = '#' + option[m] + 'multipleoption';
+        $(itemOne).show();
+        $(itemOne).delay(3000).hide(1);
+      } else {
+        if (m === 0) {
+          questionInformation = allmultiple[m].value;
+        } else {
+          questionOptions.push(option[m] + ':' + allmultiple[m].value);
+          count++;
         }
+      }
+    }
 
-        var questionInformation = '', questionOptions = [], count = 1;
-        var option = ['question', 'A', 'B', 'C', 'D'];
-
-        for (var m = 0; m < allmultiple.length; m++) {
-            if (!(allmultiple[m].value)) {
-                var itemOne = '#' + option[m] + 'multipleoption';
-                $(itemOne).show();
-                $(itemOne).delay(3000).hide(1);
-            } else {
-                if (m === 0) {
-                    questionInformation = allmultiple[m].value;
-                } else {
-                    questionOptions.push(option[m] + ':' + allmultiple[m].value);
-                    count++;
-                }
-            }
-        }
-
-        if (count === allmultiple.length) {
-            var information = {
-                questionContent: questionInformation,
-                questionKey: answer,
-                questionPoint: 1,
-                typeId: 1,
-                optionContent: questionOptions
-            };
-        }
-    });
+    if (count === allmultiple.length) {
+      var information = {
+        questionContent: questionInformation,
+        questionKey: answer,
+        questionPoint: 1,
+        typeId: 1,
+        optionContent: questionOptions
+      };
+      $("#addItem>.content").data("onData")(information);
+    }
+  });
 });
--- public/js/single-choice.js
+++ public/js/single-choice.js
@@ -1,50 +1,45 @@
-$(function () {
-    $('#singleEmpty').on('click', function () {
-        $('.input').val("");
-        [].forEach.call($("[name='singleAnswer']"), function (elem) {
-            elem.checked = false;
-        });
-    });
+$(function() {
 
-    $('#singleSure').click(function () {
-        var allsingle = $("[name='singleQuestion']");
-        var item = $("[name='singleAnswer']");
-        var answer;
-        for (var x = 0; x < item.length; x++) {
-            if (item[x].checked) {
-                answer = item[x].value;
-                break;
-            }
+  $('#singleSure').click(function() {
+    var allsingle = $("[name='singleQuestion']");
+    var item = $("[name='singleAnswer']");
+    var answer;
+    for (var x = 0; x < item.length; x++) {
+      if (item[x].checked) {
+        answer = item[x].value;
+        break;
+      }
+    }
+
+    var questionInformation = '',
+      questionOptions = [],
+      count = 1;
+    var option = ['question', 'A', 'B', 'C', 'D'];
+
+    for (var m = 0; m < allsingle.length; m++) {
+      if (!(allsingle[m].value)) {
+        var itemOne = '#' + option[m] + 'singleoption';
+        $(itemOne).show();
+        $(itemOne).delay(3000).hide(1);
+      } else {
+        if (m === 0) {
+          questionInformation = allsingle[m].value;
+        } else {
+          questionOptions.push(option[m] + ':' + allsingle[m].value);
+          count++;
         }
+      }
+    }
 
-        var questionInformation = '', questionOptions = [], count = 1;
-        var option = ['question', 'A', 'B', 'C', 'D'];
-
-        for (var m = 0; m < allsingle.length; m++) {
-            if (!(allsingle[m].value)) {
-                var itemOne = '#' + option[m] + 'singleoption';
-                $(itemOne).show();
-                $(itemOne).delay(3000).hide(1);
-            } else {
-                if (m === 0) {
-                    questionInformation = allsingle[m].value;
-                } else {
-                    questionOptions.push(option[m] + ':' + allsingle[m].value);
-                    count++;
-                }
-            }
-        }
-
-        if (count === allsingle.length) {
-            var information = {
-                questionContent: questionInformation,
-                questionKey: answer,
-                questionPoint: 1,
-                typeId: 1,
-                optionContent: questionOptions
-            };
-        }
-
-        $("#addItem>.content").data("onData")(information);
-    });
+    if (count === allsingle.length) {
+      var information = {
+        questionContent: questionInformation,
+        questionKey: answer,
+        questionPoint: 1,
+        typeId: 1,
+        optionContent: questionOptions
+      };
+      $("#addItem>.content").data("onData")(information);
+    }
+  });
 });
