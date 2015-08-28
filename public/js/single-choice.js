ccce35bbb4e6a0f64e9353d3f10e6d9fa8044755
wy <wy08271@outlook.com>
2015-08-28 23:14:31 +0800

    change single-choice.js loop use

--- public/js/single-choice.js
+++ public/js/single-choice.js
@@ -1,36 +1,32 @@
 $(function() {
 
   $('#singleSure').click(function() {
-    var questionInformation = $("[name='singleQuestion']").val();
-    var questionOption = $("[name='singleQuestionOption']");
-    var item = $("[name='singleAnswer']");
-    var answer;
-    for (var x = 0; x < item.length; x++) {
-      if (item[x].checked) {
-        answer = item[x].value;
-        break;
+    var questionInformation = $("[name='singleQuestion']").val(),answer;
+    $("[name='singleAnswer']").each(function() {
+      if (this.checked) {
+        answer = this.value;
       }
-    }
+    });
 
-    var optionInformation = [];
+    var optionInformation = [],m = 0;
     var itemOption = ['A', 'B', 'C', 'D'];
 
-    for (var m = 0; m < questionOption.length; m++) {
-      if (!(questionOption[m].value)) {
+    $("[name='singleQuestionOption']").each(function(m) {
+      if (this.value) {
+        optionInformation.push(itemOption[m] + ':' + this.value);
+      } else {
         $('#' + itemOption[m] + 'singleoption').show();
         $('#' + itemOption[m] + 'singleoption').delay(3000).hide(1);
-      } else {
-        optionInformation.push(itemOption[m] + ':' + questionOption[m].value);
       }
-    }
+    });
 
-      var information = {
-        questionContent: questionInformation,
-        questionKey: answer,
-        questionPoint: 1,
-        typeId: 1,
-        optionContent: optionInformation
-      };
-      $("#addItem>.content").data("onData")(information);
+    var information = {
+      questionContent: questionInformation,
+      questionKey: answer,
+      questionPoint: 1,
+      typeId: 1,
+      optionContent: optionInformation
+    };
+    $("#addItem>.content").data("onData")(information);
   });
 });
